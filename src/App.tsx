c8373f1ca88092c53bf59a1ac9158323f3846f68
Samuel-Lucas <samuel.moreira10@outlook.com>
2025-10-14 22:49:09 -0300

    Implementando funcionalidade Context Api

--- src/App.tsx
+++ src/App.tsx
@@ -5,27 +5,47 @@ import Account from './pages/Account';
 import { Layout } from './components/Layout';
 import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
 import AccountInfo from './pages/AccountInfo';
+import { createContext } from 'react';
+
+interface IAppContext {
+  user: string
+}
+
+export const AppContext = createContext({} as IAppContext)
+
+const AppContextProvider = ({ children }: any) => {
+
+  const user = 'Sam'
+
+  return (
+    <AppContext.Provider value={{ user }}>
+      { children }
+    </AppContext.Provider>
+  )
+}
 
 function App() {
   return (
     <BrowserRouter>
-      <ChakraProvider value={defaultSystem}>
-        <Layout>
-          <Routes>
-            <Route path='/' element={
-              <Home />
-            }>
-            </Route>
-            <Route path='/conta/:id' element={
-              <Account />
-            }>
-            </Route>
-            <Route path='/accountinfo' element={
-              <AccountInfo />
-            }></Route>
-          </Routes>
-        </Layout>
-      </ChakraProvider>
+      <AppContextProvider>
+        <ChakraProvider value={defaultSystem}>
+          <Layout>
+            <Routes>
+              <Route path='/' element={
+                <Home />
+              }>
+              </Route>
+              <Route path='/conta/:id' element={
+                <Account />
+              }>
+              </Route>
+              <Route path='/accountinfo' element={
+                <AccountInfo />
+              }></Route>
+            </Routes>
+          </Layout>
+        </ChakraProvider>
+      </AppContextProvider>
     </BrowserRouter>
   );
 }
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,13 @@
+import { useContext } from 'react'
 import './Header.css'
+import { AppContext } from '../../App'
 
 export const Header = () => {
+
+    const context = useContext(AppContext)
+
+    console.log(context)
+    
     return(
         <div className='header'>
             Dio Bank
